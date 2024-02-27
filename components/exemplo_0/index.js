001f32cd863d100bd792020405a01ed2933a17e7
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-02-27 15:24:18 -0300

    aula27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'; 
+
+function Exemplo_0 () {
+    return (
+        <View>
+             <Text>Minha primera tela</Text>
+
+        </View>
+
+    );
+};
+
+export default Exemplo_0;
