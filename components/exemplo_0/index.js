e1f787a2f454d4a32ba92a7e3248c6c158196504
LAURA PEREIRA DE AGUIAR <laura.aguiar3>
2024-03-05 15:25:16 -0300

    aula 05-03

--- components/exemplo_0/index.js
+++ components/exemplo_0/index.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'; 
 
+import styles from './styles';
+
 function Exemplo_0 () {
     return (
-        <View>
-             <Text style={{color:'#fff'}}>Minha primera tela</Text>
+        <View style={styles.container}>
+             <Text> Minha primera tela</Text>
 
         </View>
 
