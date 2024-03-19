691d5d8a63c3caabd599ec33588a8943b139d483
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-03-19 15:23:25 -0300

    aula 19-03

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Exemplo_0 from './components/exemplo_1';
 
+import Exemplo2 from './components/exemplo_2';
+
 import Atividade_1 from './components/atividade_1';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade_1/>
+      <Exemplo2/>
       <StatusBar style="auto" />
     </View>
   );
