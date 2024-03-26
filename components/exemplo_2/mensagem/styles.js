28aa32182c0e221d805403b248fca09d7c48b2b9
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-03-26 15:25:20 -0300

    aula 26-03

--- components/exemplo_2/mensagem/styles.js
+++ components/exemplo_2/mensagem/styles.js
@@ -1,22 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#beb9fa',
-       height: '100%',
-        width:'100%',
-        borderRadius: 20,
-        alignItems: 'center',
-        padding: 8,
-    },
-
-    titulo: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginVertical: 20,
-
-    },
-
+   
     mensagem: {
         fontSize: 25,
         margin: 10,
