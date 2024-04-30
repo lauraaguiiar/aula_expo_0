407735f8d176a7da14f3a83aa252da92ee6e1fda
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-04-30 15:25:25 -0300

    30-04-2024

---
+++ components/exemplo_3/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e67a96',
+        borderRadius: 20,
+    },
+
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 20,
+        color: '#f53165',
+
+    },
+
+
+})
+
+export default styles;
