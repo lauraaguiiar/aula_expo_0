ff09bb74ba975fa4316b9337c4baebca0076a2ca
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-05-03 09:45:32 -0300

    03-05-2024

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c9acfa',
+    backgroundColor: '#e80744',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 18,
--- components/exemplo_3/styles.js
+++ components/exemplo_3/styles.js
@@ -3,16 +3,26 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#e67a96',
+        backgroundColor: '#ff9cb7',
         borderRadius: 20,
+        width:'100%',
+        alignItems: 'center',
     },
 
     titulo: {
         fontSize: 30,
         fontWeight: 'bold',
         marginVertical: 20,
-        color: '#f53165',
+        color: '#e80744',
 
+    },
+
+    botao:{
+        borderWidth:3,
+        padding: 10,
+        width: '80%',
+        borderRadius: 20,
+        alignItems: 'center',
     },
 
 
