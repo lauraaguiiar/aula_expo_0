69304db475f69ee714f30f7ce54325ff30e205a4
lauraaguiiar <160647009+lauraaguiiar@users.noreply.github.com>
2024-05-10 09:47:44 -0300

    aula 10-05-2024

--- components/exemplo_3/index.js
+++ components/exemplo_3/index.js
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import { View, Text, Pressable } from 'react-native';
 
 import styles from './styles';
 
 export default function Exemplo_3 () {
+
+    const [numero, setNumero] = useState(0);
+
+    function incremento () {
+       setNumero(numero + 1);
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}> Exemplo_3  </Text> 
-            <Pressable onPress={() =>alert('Oi')} style={styles.botao}>
-                <Text> Ex. botão</Text>
+
+            <Pressable 
+            onPress={() =>alert('Oi')} 
+            style={ ( {pressed}) => pressed?
+            [ styles.botao, styles.botaoPress]
+            :
+            styles.botao}
+            >
+                
+                <Text style={styles.txtBotao}> Ex. botão</Text>
+
             </Pressable>
+
+            <Text style={styles.titulo}> Exemplo state  </Text> 
+
+            <Text style={styles.txtNumero}> { numero } </Text>
+
+            <Pressable 
+                onPress={() =>incremento()} 
+            style={ ( {pressed}) => pressed?
+            [ styles.botao, styles.botaoPress]
+            :
+            styles.botao}
+            >
+                
+                <Text style={styles.txtBotao}> +1 </Text>
+
+            </Pressable>
+
         </View>
     );
 }
--- components/exemplo_3/styles.js
+++ components/exemplo_3/styles.js
@@ -1,5 +1,6 @@
 import { StyleSheet } from 'react-native';
 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -23,8 +24,24 @@ const styles = StyleSheet.create({
         width: '80%',
         borderRadius: 20,
         alignItems: 'center',
+        borderColor: '#cc3fac',
+    },
+    
+    txtBotao:{
+        fontSize: 20,
+        color:'#cc3fac'
     },
 
+     botaoPress:{
+        backgroundColor: '#734af0'
+
+    },
+
+    txtNumero:{
+       fontSize: 35,
+       color: '#cc3fac',
+       marginVertical:10,
+    },
 
 })
 
